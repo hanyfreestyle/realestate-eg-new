676e836d95af4a3ca6298c20b204ce88ce3ecf88
Hany Darwish <hany.freestyle4u@gmail.com>
2025-03-20 00:57:42 +0200

    Update

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,6 +3,7 @@
 namespace App\Providers;
 
 use BezhanSalleh\FilamentLanguageSwitch\LanguageSwitch;
+use Illuminate\Support\Facades\URL;
 use Illuminate\Support\ServiceProvider;
 use Illuminate\Support\Facades\File;
 use Illuminate\Support\Facades\App;
@@ -13,7 +14,6 @@ class AppServiceProvider extends ServiceProvider {
 #@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
 #||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||
     public function register(): void {
-
         $adminRoutesPath = base_path('app/Helpers/function');
         if (File::isDirectory($adminRoutesPath)) {
             foreach (File::allFiles($adminRoutesPath) as $file) {
@@ -28,6 +28,7 @@ class AppServiceProvider extends ServiceProvider {
 #@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
 #||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||
     public function boot(): void {
+        URL::forceScheme('https');
         LanguageSwitch::configureUsing(function (LanguageSwitch $switch) {
             $switch->locales(['ar', 'en']); // also accepts a closure
 //            dd('hany');
