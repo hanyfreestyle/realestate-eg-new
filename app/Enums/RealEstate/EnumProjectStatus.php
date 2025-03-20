5a9a154b4625091b1a87dc6f3df218744377329f
Hany Darwish <hany.freestyle4u@gmail.com>
2025-03-20 16:09:11 +0200

    Update

--- app/Enums/RealEstate/EnumProjectStatus.php
+++ app/Enums/RealEstate/EnumProjectStatus.php
@@ -10,7 +10,6 @@ enum EnumProjectStatus: string {
     case UnderConstruction = 'under-construction';
     case Completed = 'completed';
 
-
     public function label(): string {
         return match ($this) {
             self::UnderConstruction => __('filament/RealEstate/listing.status_options.under-construction'),
